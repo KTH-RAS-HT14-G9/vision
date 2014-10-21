076eb1c804deeed6ee5d8f764d0badd80e4bfe12
tsebastiao <tiago.sebastiao@gmail.com>
2014-10-21 15:47:11 +0200

    update object detection - v2

--- object_detection/src/object_depth.cpp
+++ object_detection/src/object_depth.cpp
@@ -1,16 +1,21 @@
-
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <math.h>
 #include <std_msgs/Float64.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include "opencv2/features2d/features2d.hpp"
+#include <opencv2/objdetect/objdetect.hpp>
+
 
 const int _frequency=10;
 cv_bridge::CvImagePtr _cv_ptr;
 const int _height=240;
 const int _width=320;
-const int _small=10;//15
+const int _small=5; //10 , 15
 float _average;
 
 namespace enc = sensor_msgs::image_encodings;
@@ -18,11 +23,19 @@ namespace enc = sensor_msgs::image_encodings;
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
     float matrix[_width][_height];
+    int mask[_small][_small]={1,4,7,4,1,
+                             4,16,26,16,4,
+                             7,26,41,26,7,
+                             4,16,26,16,4,
+                             1,4,7,4,1};
 
     //ROS_INFO("%d , %d", msg->width,msg->height);
     try
     {
         _cv_ptr = cv_bridge::toCvCopy(msg, enc::TYPE_32FC1);
+
+        cv::Mat mat = _cv_ptr->image;
+
 
         int startx=(msg->width-_width)/2;
         int endx=startx+_width;
@@ -52,20 +65,24 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
         ROS_INFO("Min at: %d %d",i,j);
 
         float sum=0;
-        int N=_small*_small;
-        for(int x=i-_small/2;x<i+_small/2;x++){
-            for(int y=j-_small/2;y<j+_small/2;y++){
+        //int N=_small*_small;
+        int N=273; // sum of weights of the mask
+        for(int x=i-(_small-1)/2;x<i+(_small-1)/2;x++){
+            for(int y=j-(_small-1)/2;y<j+(_small-1)/2;y++){
                 if(!(x>=0 && x < _width && y >= 0 && y < _height)){
                     //ROS_ERROR("Out bounds: %d %d %d %d",x+startx-1,y+starty-1,x,y);
-                    N--;
+                    //N--;
+                    N=N-mask[x-i+(_small-1)/2][y-j+(_small-1)/2];
                 }
                 else{
 
                     if(std::abs(matrix[x][y]) < 10 && std::abs(matrix[x][y])<=1.5*min)
-                        sum=sum+matrix[x][y];
+                        //sum=sum+matrix[x][y];
+                        sum=sum+mask[x-i+(_small-1)/2][y-j+(_small-1)/2]*matrix[x][y];
                     else{
                         //ROS_ERROR("Bad value:  %d %d %d %d",x+startx-1,y+starty-1,x,y);
-                        N--;
+                        //N--;
+                        N=N-mask[x-i+(_small-1)/2][y-j+(_small-1)/2];
                     }
 
                 }
@@ -73,7 +90,10 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
         }
 
          _average=sum/N;
-         if(_average>1.5 ||  _average<0.3){
+         if(_average>2 ||  _average<0.3){
+             _average=NAN;
+         }
+         if(!(_average>=0.8*matrix[i][j] && _average<=1.2*matrix[i][j])){
              _average=NAN;
          }
          //ROS_INFO("Average: %f",_average);
@@ -91,9 +111,14 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
             }
          */
 
-        float depth = _cv_ptr->image.at<float>(cv::Point(320,240));
+        //float depth = _cv_ptr->image.at<float>(cv::Point(50,50));
 
-        ROS_INFO("Depth: %f", depth);
+        //ROS_INFO("Depth: %f", depth);
+
+        cv::namedWindow( "Window depth", cv::WINDOW_AUTOSIZE );// Create a window for display.
+        cv::imshow( "Window depth", mat );
+        cv::waitKey(0);
+
 
     }
     catch (cv_bridge::Exception& e)
@@ -111,7 +136,10 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
 
     image_transport::ImageTransport it(n);
-    image_transport::Subscriber sub = it.subscribe("/camera/depth/image", 1, imageCallback);
+    image_transport::Subscriber sub1 = it.subscribe("/camera/depth/image", 1, imageCallback);
+
+    //image_transport::Subscriber sub2 = it.subscribe("/camera/rgb/image_raw", 1, imageRGBCallback);
+
     ros::Publisher depth_pub=n.advertise<std_msgs::Float64>("/object_distance",1000);
 
     ros::Rate loop_rate(_frequency);
