e5f29be1c0a85827bea358d10420a1d6b7d94ab7
Max Losch <mmlosch@kth.se>
2014-11-01 22:32:07 +0100

    working wall detector. works sucessfully on test cloud

--- wall_detector/src/wall_detector.cpp
+++ wall_detector/src/wall_detector.cpp
@@ -27,8 +27,51 @@ void callback_point_cloud(const WallExtractor::SharedPointCloud& pcloud)
 //------------------------------------------------------------------------------
 // Entry point
 
+void testcase() {
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    // Fill in the cloud data
+    cloud->width  = 150+300+150;
+    cloud->height = 1;
+    cloud->points.resize (cloud->width * cloud->height);
+
+    // Generate the data
+    int k = 0;
+    // Generate bottom
+    for (size_t i = 0; i < 300; ++i)
+    {
+        cloud->points[i].x = 60 * rand () / (RAND_MAX + 1.0f);
+        cloud->points[i].y = 30 * rand () / (RAND_MAX + 1.0f);
+        cloud->points[i].z = 0.0;
+    }
+    k+=300;
+
+    // Generate left wall
+    for (size_t i = 0; i < 150; ++i)
+    {
+        cloud->points[k+i].x = 60 * rand () / (RAND_MAX + 1.0f);
+        cloud->points[k+i].y = -1;
+        cloud->points[k+i].z = 20 * rand () / (RAND_MAX + 1.0f);
+    }
+    k+=150;
+
+    // Generate right wall
+    for (size_t i = 0; i < 150; ++i)
+    {
+        cloud->points[k+i].x = 60 * rand () / (RAND_MAX + 1.0f);
+        cloud->points[k+i].y = +1;
+        cloud->points[k+i].z = 20 * rand () / (RAND_MAX + 1.0f);
+    }
+
+    _extractor.extract(cloud);
+}
+
 int main(int argc, char **argv)
 {
+    testcase();
+    return 0;
+
     ros::init(argc, argv, "wall_detector");
 
     ros::NodeHandle n;
