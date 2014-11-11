b640c2c72a83f53847754bd4b10691c9517438c4
Max Losch <mmlosch@kth.se>
2014-11-12 00:32:21 +0100

    sphere recognition done

--- object_recognition/include/shape_fitting/shape_fitting.h
+++ object_recognition/include/shape_fitting/shape_fitting.h
@@ -2,6 +2,8 @@
 #define SHAPE_FITTING_H
 
 #include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
 #include <common/types.h>
 
 class ShapeFitting
@@ -9,12 +11,20 @@ class ShapeFitting
 public:
     ShapeFitting(pcl::SacModel model);
 
-    double classify(const common::SharedPointCloudRGB& cloud);
+    double classify(const common::SharedPointCloudRGB& cloud, pcl::ModelCoefficients& coefficients);
+
+    void set_segmentation(double dist_thresh, double normal_dist_weight, double min_r, double max_r);
+    void set_normal_estimation(int k);
 
 protected:
     pcl::PointIndices _inliers;
-    pcl::ModelCoefficients _coefficients;
-    pcl::SACSegmentation<pcl::PointXYZRGB> _seg;
+
+    //pcl::SACSegmentation<pcl::PointXYZRGB> _seg;
+    pcl::SACSegmentationFromNormals<pcl::PointXYZRGB, pcl::Normal> _seg;
+    pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> _normal_est;
+
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr _kd_tree;
+    pcl::PointCloud<pcl::Normal>::Ptr _normals;
 };
 
 #endif // SHAPE_FITTING_H
--- object_recognition/src/object_recognition.cpp
+++ object_recognition/src/object_recognition.cpp
@@ -2,9 +2,29 @@
 #include <object_detector/ROI.h>
 #include <shape_fitting/shape_fitting.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <common/parameter.h>
+#include <common/debug.h>
+
+#if ENABLE_VISUALIZATION_RECOGNITION==1
+#include <pcl/visualization/pcl_visualizer.h>
+#include <common/visualization_addons.h>
+#endif
+
+
+Parameter<double> _sphere_min_r("/vision/recognition/sphere/min_radius", 0.03/2.0);
+Parameter<double> _sphere_max_r("/vision/recognition/sphere/max_radius", 0.07/2.0);
+Parameter<int> _sphere_kd_k("/vision/recognition/sphere/kd_k", 25);
+Parameter<double> _sphere_dist_thresh("/vision/recognition/sphere/dist_thresh", 0.05);
+Parameter<double> _sphere_normal_weight("/vision/recognition/sphere/normal_dist_weight", 0.1);
 
 ShapeFitting _sphere_fitter(pcl::SACMODEL_SPHERE);
 std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr > _clouds;
+pcl::ModelCoefficients _model_coefficients;
+
+#if ENABLE_VISUALIZATION_RECOGNITION==1
+pcl::visualization::PCLVisualizer _viewer("Recognition");
+common::Colors _colors;
+#endif
 
 void callback_rois(const object_detector::ROIConstPtr& rois)
 {
@@ -25,17 +45,48 @@ int main(int argc, char **argv)
 
     ros::NodeHandle n;
 
-    ros::Subscriber sub_rois = n.subscribe<object_detector::ROI>("/vision/obstacle/rois",1,callback_rois);
+    ros::Subscriber sub_rois = n.subscribe<object_detector::ROI>("/vision/obstacles/rois",1,callback_rois);
+
+#if ENABLE_VISUALIZATION_RECOGNITION==1
+    _viewer.addCoordinateSystem (1.0);
+    _viewer.initCameraParameters ();
+#endif
 
 
     ros::Rate rate(10);
+    common::Timer timer;
     while(n.ok()) {
+
+        _sphere_fitter.set_segmentation(_sphere_dist_thresh(), _sphere_normal_weight(), _sphere_min_r(), _sphere_max_r());
+        _sphere_fitter.set_normal_estimation(_sphere_kd_k());
+
+        timer.start();
+
+#if ENABLE_VISUALIZATION_RECOGNITION==1
+    _viewer.removeAllPointClouds();
+    _viewer.removeAllShapes();
+    _colors.reset();
+
+#endif
 
         for(int i = 0; i < _clouds.size(); ++i)
         {
-            double prob = _sphere_fitter.classify(_clouds[i]);
+            double prob = _sphere_fitter.classify(_clouds[i],_model_coefficients);
             std::cerr << "Probability: " << prob << std::endl;
+
+#if ENABLE_VISUALIZATION_RECOGNITION==1
+            std::stringstream ss;
+            ss << "Cloud_" << i;
+            common::Color c = _colors.next();
+            pcl::visualization::AddPointCloud(_viewer,_clouds[i],ss.str(),c.r,c.g,c.b);
+            _viewer.addSphere(_model_coefficients,"sphere");
+
+            _viewer.spinOnce(100);
+#endif
+
         }
+
+        ROS_INFO("Recognition - Time: %lf",timer.elapsed());
 
         std::cerr << "---------------------------------------------" << std::endl << std::endl;
 
