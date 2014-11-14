181d2e38b3159975efb31ee34ed006b86a24edfc
Max Losch <mmlosch@kth.se>
2014-11-14 16:10:40 +0100

    make sure we do not put out rois when there is no ground visible

--- object_detector/src/roi_extractor.cpp
+++ object_detector/src/roi_extractor.cpp
@@ -134,6 +134,18 @@ common::vision::ROIArrayPtr ROIExtractor::extract(
         //std::cerr << "Wall " << i << ", Metric: " << metric << std::endl;
     }
 
+    std::cout << "Ground metric: " << max_metric << std::endl;
+
+    //check if it's the true ground (happens only when we do not see the ground)
+    if (max_metric < 0.8) {
+        ground_plane = -1;
+        std::cerr << "-----------> No ground to see <-----------" << std::endl;
+        std::cerr << "Greatest metric: " << max_metric << std::endl << std::endl;
+
+        //return no rois. We cannot distinguish objects.
+        return rois;
+    }
+
 #if ENABLE_VISUALIZATION_ROIS==1
 
     std::stringstream ss;
